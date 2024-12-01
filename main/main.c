6c5d79b0a6c617a1f73e8aa3a4cb73fd0e59f97e
Patryk Monarcha <s171552@student.pg.edu.pl>
2024-12-01 16:57:21 +0100

    34 Przygotowanie podstawy do projektu na MCU

---
+++ main/main.c
@@ -0,0 +1,75 @@
+/**
+  **********************************************************************************************************************
+  * @file    main.c
+  * @brief   This file is the entry file for the device's firmware
+  * @authors patrykmonarcha
+  * @date Dec 1, 2024
+  **********************************************************************************************************************
+  */
+
+/* Includes -------------------------------------------------------------------------------------------------*/
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+#include "esp_log.h"
+
+/* Private typedef ---------------------------------------------------------------------------------------------------*/
+
+/* Private define ----------------------------------------------------------------------------------------------------*/
+
+/* Private macros ----------------------------------------------------------------------------------------------------*/
+
+/* Private variables -------------------------------------------------------------------------------------------------*/
+static const char * TAG = "app_main";
+
+/* External variables ------------------------------------------------------------------------------------------------*/
+TaskHandle_t xChipInfoHandle = NULL;
+
+/* Private function declarations -------------------------------------------------------------------------------------*/
+
+/* Private function definitions --------------------------------------------------------------------------------------*/
+
+/* Exported function definitions -------------------------------------------------------------------------------------*/
+void vChipInfoTask(void * pvParameters) {
+    while(1) {
+        /* Print chip information */
+        esp_chip_info_t chip_info;
+        uint32_t flash_size;
+        esp_chip_info(&chip_info);
+        printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
+               CONFIG_IDF_TARGET,
+               chip_info.cores,
+               (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
+               (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
+               (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
+               (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
+
+        unsigned major_rev = chip_info.revision / 100;
+        unsigned minor_rev = chip_info.revision % 100;
+        printf("silicon revision v%d.%d, ", major_rev, minor_rev);
+        if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
+            printf("Get flash size failed");
+            return;
+        }
+
+        printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
+               (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+        vTaskDelay(10000 / portTICK_PERIOD_MS);
+    }
+}
+
+void app_main(void)
+{
+
+    ESP_LOGI(TAG, "Starting app");
+
+    xTaskCreate(vChipInfoTask, "CHIPINFO", 2048, NULL, tskIDLE_PRIORITY + 1, &xChipInfoHandle);
+}
+
+/* END OF FILE -------------------------------------------------------------------------------------------------------*/
