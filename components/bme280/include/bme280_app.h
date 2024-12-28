b38ed4ca9510bb168c4812a11c6339a146a93609
Julia Hrycyna <s193272@student.pg.edu.pl>
2024-12-28 12:14:52 +0100

    43 dodanie do API odczytu temperatury z czujnika BME280
    dodanie komentarzy, usunięcie niepotrzebnych wcięć

--- components/bme280/include/bme280_app.h
+++ components/bme280/include/bme280_app.h
@@ -41,7 +41,20 @@ extern "C" {
  */
 esp_err_t initializeBME280Device(bme280_t ** bme280, i2c_master_bus_handle_t i2c_bus_handle);
 
-    esp_err_t getBME280Temperature(bme280_t * bme280, float *temperature);
+/*
+ * @function getBME280Temperature
+ *
+ * @abstract This function reads BME280 sensor temperature
+ *
+ * @param[in] bme280: BME280 instance
+ *
+ * @param[out] temperature: Temperature value
+ *
+ * @return
+ *    - esp_err_t status code
+ */
+
+esp_err_t getBME280Temperature(bme280_t * bme280, float *temperature);
 
 #ifdef __cplusplus
 }
