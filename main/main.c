f9bdb5a711a01ce67d6d898c5889fc4cbf53b9ec
Patryk Monarcha <s171552@student.pg.edu.pl>
2025-03-22 01:39:45 +0100

    transmisja ble w czasie rzeczywistym

--- main/main.c
+++ main/main.c
@@ -31,7 +31,7 @@
 /* Private macros ----------------------------------------------------------------------------------------------------*/
 #define MEASUREMENTS_FREQUENCY 10 // [Hz]
 #define MEASUREMENTS_DELAY_MS (1000 / MEASUREMENTS_FREQUENCY) // [ms]
-#define BLE_STREAM_DELAY_MS 500 // [ms]
+#define BLE_STREAM_DELAY_MS 10 // [ms]
 
 /* Private variables -------------------------------------------------------------------------------------------------*/
 static const char * TAG = "MAIN";
@@ -59,14 +59,17 @@ void vBME280Task(void * pvParameters) {
 
     while (1) {
 
-        getBME280Temperature(bme280, &temperatureReading);
-        save_temperature(temperatureReading);
+        do {
+            getBME280Temperature(bme280, &temperatureReading);
+            save_temperature(temperatureReading);
 
-        getBME280Humidity(bme280, &humidityReading);
-        save_humidity(humidityReading);
+            getBME280Humidity(bme280, &humidityReading);
+            save_humidity(humidityReading);
 
-        getBME280Pressure(bme280, &pressureReading);
-        save_pressure(pressureReading);
+            getBME280Pressure(bme280, &pressureReading);
+            save_pressure(pressureReading);
+
+        } while(isBME280Sampling(bme280));
 
         vTaskDelay(MEASUREMENTS_DELAY_MS / portTICK_PERIOD_MS);
     }
@@ -98,7 +101,6 @@ void app_main(void) {
 
     ESP_LOGI(TAG, "Starting BME280 task");
     xTaskCreate(vBME280Task, "BME280", 8192, NULL, tskIDLE_PRIORITY + 1, &xBME280Handle);
-
     ESP_LOGI(TAG, "Starting BLE stream task");
     xTaskCreate(vBLEStreamTask, "BLESTREAM", 4096, NULL, tskIDLE_PRIORITY, &xBLEStreamHandle);
 
